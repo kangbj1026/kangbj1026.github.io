2efae70c76dc5944bcbcaeb6fe8f6e7edf36c803
kangbj1026 <51243212+kangbj1026@users.noreply.github.com>
2022-06-09 00:01:08 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let para = document.location.href.split("/");
-// let linkName = para[4].replace(/(.html)$/,''); // localhost 적용
-let linkName = para[3].replace(/(.html)$/,''); // github 적용
+let linkName = para[4].replace(/(.html)$/,''); // localhost 적용
+// let linkName = para[3].replace(/(.html)$/,''); // github 적용
 
 let wrapper = document.getElementById('wrapper');
 
@@ -10,13 +10,19 @@ wrapperDivLeft.classList.add('wrapper-left');
 let wrapperDivRight = document.createElement('div');
 wrapperDivRight.classList.add('wrapper-right');
 
-let RightList 		= document.createElement('p');
 let leftListContent = document.createElement('span');
 
+let RightList 		= document.createElement('p');
+
+let RightListIntroduction = document.createElement('h1');
+let RightListMyName = document.createElement('p');
+let RightListDateBirth = document.createElement('p');
+let RightListResidence = document.createElement('p');
+let RightListCareer = document.createElement('p');
+let RightListLanguageOfUse = document.createElement('h1');
+
 let RightListEmail 	= document.createElement('p');
-
 let RightListPhone 	= document.createElement('p');
-
 let RightListGithub = document.createElement('span');
 let RightListLink	= document.createElement('a');
 
@@ -35,10 +41,18 @@ if (linkName == "contact") {
 	RightListGithub.appendChild(RightListLink);
 } else if (linkName == "projects") {
 	leftListContent.innerHTML = "<h3>"+linkName+"</h3>";
-	RightList.appendChild(leftListContent);
+
 } else {
 	leftListContent.innerHTML = "<h3>Hello :) </h3>";
-	RightList.appendChild(leftListContent);
+
+	RightListIntroduction.innerHTML = "소개";
+	RightListMyName.innerHTML = "이름 : 강 병 주 ( 姜 秉 周 )";
+	RightListMyName.style.fontSize = "1.5rem";
+	RightListDateBirth.innerHTML = "생년월일 : 1987 / 06 / 06 ( 만35 )";
+	RightListResidence.innerHTML = "주소 : 인천 계양구";
+	RightListCareer.innerHTML = "경력 : 웹 개발 1년";
+
+	RightListLanguageOfUse.innerHTML = "사용 언어";
 }
 
 wrapperAll();
@@ -57,6 +71,13 @@ function wrapperLeftLine() {
 
 function wrapperRightLine() {
 	wrapperDivRight.appendChild(RightList);
+
+	RightList.appendChild(RightListIntroduction);
+	RightList.appendChild(RightListMyName);
+	RightList.appendChild(RightListDateBirth);
+	RightList.appendChild(RightListResidence);
+	RightList.appendChild(RightListCareer);
+	RightList.appendChild(RightListLanguageOfUse);
 
 	RightList.appendChild(RightListEmail);
 	RightList.appendChild(RightListPhone);
