210bd52aec4ff77f2999b096653eba86aa5d8bb7
kangbj1026 <51243212+kangbj1026@users.noreply.github.com>
2022-06-13 17:26:11 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+document.title = "ByungJu Kang's Portfolio";
 let para = document.location.href.split("/");
 // let linkName = para[3].replace(/(.html)$/,''); // localhost 적용
 let linkName = para[3].replace(/(.html)$/,''); // github 적용
