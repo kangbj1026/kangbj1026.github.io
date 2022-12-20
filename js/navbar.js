4071b29086b585641f0eac60082dd91468dd4e2e
kangbj1026 <51243212+kangbj1026@users.noreply.github.com>
2022-12-20 15:54:52 +0900

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -3,8 +3,8 @@ function navItems() {
         ['Introduce', './index.html'],
         ['Projects', './projects.html'],
         ['Contact', './contact.html'],
-        ['StudyIng', './studying.html'],
-        ['SpringBootMVC', './portfolio']
+        ['StudyIng', './studying.html']
+//         ['SpringBootMVC', './portfolio']
     ];
 
     let navbar = document.getElementsByClassName('navbar');
